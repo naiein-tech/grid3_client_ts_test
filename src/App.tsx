086cc8d303480c4705aaad5c246ad17a16fee605
naiein-tech <toto@naiein.com>
2023-03-15 20:31:20 +0100

    webpack5 errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import { GridClient, NetworkEnv } from "grid3_client";
+import { MessageBusClient } from "ts-rmb-redis-client";
+
+type TFProfile = {
+    id: string;
+    name: string;
+    mnemonics: string;
+    storeSecret: string;
+    networkEnv: string;
+    sshKey: string;
+}
+
+async function getGridClient(profile: TFProfile): Promise<GridClient> {
+    const rmb = new MessageBusClient();
+
+    const gridClient = new GridClient(
+        NetworkEnv.main,
+        profile.mnemonics,
+        profile.storeSecret,
+        rmb,
+    );
+    await gridClient.connect();
+    return gridClient;
+}
 
 function App() {
+  
+  let test_profile: TFProfile = {
+        id: "8623",
+        name: "main_test",
+        mnemonics: "fog hungry organ father ice dynamic camp resist goat faith maximum hire",
+        storeSecret: "test",
+        networkEnv: "main",
+        sshKey: "test",
+  }
+
+  async function getBalance() {
+      let client = await getGridClient(test_profile);
+      console.log("Getting balance");
+      client.balance.getMyBalance().then((balance) => { console.log(balance) });
+  }
+
+  useEffect (() => {
+      getBalance();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
